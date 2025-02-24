0db52360341b321d5ca3b4e4bf565d5ce75a90aa
eddiemws <eddiemws@gmail.com>
2025-02-25 01:31:14 +0300

    changed my related products

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './RelatedProducts.css'
-import data_product from '../Assets/data'
+// import data_product from '../Assets/data'
 import Item from '../Item/Item'
+import { allItems } from '../CategorySection/Category'
 
 const RelatedProducts = () => {
   return (
@@ -9,8 +10,8 @@ const RelatedProducts = () => {
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_product.map((item,i)=> {
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        {allItems.slice(0,4).map((item,i)=> {
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.price} old_price={item.old_priceprice}/>
 
         })}
 
